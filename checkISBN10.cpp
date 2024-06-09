148a880e4a0c7da2f402c182ea54faa445167946
rahul-797 <rahulsangwan797@gmail.com>
2024-06-09 11:30:05 +0530

    initial commit

---
+++ checkISBN10.cpp
@@ -0,0 +1,15 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string isbn;
+  cout << "Enter ISBN-10 digits: ";
+  cin >> isbn;
+  int sum = 0;
+  for (int i = 0; i <= 10; i++) {
+  }
+
+  return 0;
+}
